08a74bf3e774a6c289ad05d746459c70ee6ac4b2
regal-rahul <regalrahul777@gmail.com>
2022-07-26 12:23:46 +0530

    auto prefixer gulp

--- Auto Prefixer/gulpfile.js
+++ Auto Prefixer/gulpfile.js
@@ -3,12 +3,12 @@ var autoprefixer = require('gulp-autoprefixer');
 
 
 gulp.task('styles',function() {
-  gulp.src('css/styles.css')
+  gulp.src('./css/styles.css')
     .pipe(autoprefixer())
     .pipe(gulp.dest('build'))
 });
 
 
-gulp.task('watch',function() {
-  gulp.watch('css/styles.css', ['styles']);
+gulp.task('watch', () => {
+  gulp.watch('./css/styles.css', gulp.series('styles'));
 });
